853e646316b3c2cbf93b712d3a18a183d0aa8d2e
stersylva <stersylva@gmail.com>
2019-10-21 21:59:11 -0300

    projeto inicial

---
+++ src/main/java/TesteGoogle.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteGoogle {
+
+    @Test
+    public void pesquisarItem (){
+
+        // abrindo o navegador
+
+        System.setProperty("webdriver.chrome.driver", "/home/stefania/Projetos/web/chromedriver");
+        WebDriver navegador = new ChromeDriver();
+        navegador.manage().window().maximize();
+
+       navegador.get("file:///home/stefania/Projetos/Desafio_paginas_da_web/componentes.html");
+   }
+
+
+}
